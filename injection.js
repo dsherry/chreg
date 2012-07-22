db2adc74f6b5281027126d23aa8e404f6e88480a
Dylan Sherry <sharshofski@gmail.com>
2012-07-21 23:45:40 -0400

    Moved injection to file. No regex yet. Works.

---
+++ injection.js
@@ -0,0 +1,14 @@
+// Author: Dylan Sherry (email:sharshofski@gmail.com)
+// Released in 2012 under the MIT license
+
+// this code is injected into the current tab
+
+// a debugging test
+document.body.innerHTML = document.body.innerHTML + " TEST.";
+
+// get the search pattern from the text box
+var pattern = scriptOptions['sel'];
+
+
+// EOF debugging
+document.body.bgColor = 'green';
--- popup.js
+++ popup.js
@@ -1,19 +1,39 @@
+// Author: Dylan Sherry (email:sharshofski@gmail.com)
+// Released in 2012 under the MIT license
+
+// this is the main popup script
+
 function loader() {
-    document.getElementById('highlight0').onclick = addWords;
+    document.getElementById('highlight0').onclick = highlightSelection;
     console.log("loader() finished");
 };
 
-function addWords () {
-    var selection = document.getElementById('text0').value;
-    // TODO figure out this formatting bit. Then format the text0 value into the executeScript call.
+
+var uid = 'se4567ujhgfrt';
+var highlightFront = "<SPAN style='BACKGROUND-COLOR: #ffff00' name='" + uid + "'>";
+var highlightBack = "</SPAN name='" + uid + "'>"
+
+
+// example function: add selected words to the current page
+function addSelection () {
     var selection = document.getElementById('text0').value;
     console.log(selection);
-    var injection = "document.bgColor='green'; document.body.innerHTML = document.body.innerHTML + 'aasdas'; document.body.innerHTML = document.body.innerHTML + \"" + selection + "\";";
+    var injection = "document.bgColor='green'; \
+var x = 'hello'; \
+document.body.innerHTML = document.body.innerHTML + x + \"" + selection + "\"; \
+";
     chrome.tabs.executeScript(null, {code:injection});
     console.log(injection);
+};
 
+
+// highlight all occurences of the selected words in the current page
+function highlightSelection () {
+    var selection = document.getElementById('text0').value;
+    console.log(selection);
+    chrome.tabs.executeScript(null, {code: 'var scriptOptions = {sel:\"'+selection+'\"}'}, function () {
+	    chrome.tabs.executeScript(null, {file: "injection.js"});
+	});
 };
 
 window.onload = loader;
-
-console.log("fine");
